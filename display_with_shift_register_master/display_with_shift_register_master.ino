5b6000d8618fd594899b63e6214ed6b197148be1
jonas <jonasgarsten@gmail.com>
2024-11-25 22:25:40 +0100

    Adds alarm functionality

--- display_with_shift_register_master/display_with_shift_register_master.ino
+++ display_with_shift_register_master/display_with_shift_register_master.ino
@@ -7,17 +7,30 @@ const int kTimeout = 500;
 
 #define SETTINGS 2
 #define ACTION   3
+#define ALARM    4
 
 enum class Mode : unsigned int {
   Normal = 0,
   TimeMinutes = 1,
   TimeHours = 2,
-  Brightness = 3,
+  AlarmMinutes = 3,
+  AlarmHours = 4,
+  Brightness = 5,
 };
 
-time_t t_{now()};
+time_t time_{now()};
+
 Mode mode_{Mode::Normal};
 unsigned int brightness_{20};
+struct Alarm {
+  time_t alarm_time{0};
+  bool alarm_active{false};
+  time_t alarm_off;
+};
+
+Alarm alarm_;
+void SendTime(time_t t);
+void SendBrightness(byte brightness);
 
 class Button {
   public:
@@ -45,33 +58,72 @@ class Button {
 };
 
 void ActionFunction() {
-  if (mode_ == Mode::TimeMinutes) {
-    t_ += 1;
-    setTime(t_);
-    SendTime(t_);
-  } else if (mode_ == Mode::TimeHours) {
-    t_ += 60;
-    setTime(t_);
-    SendTime(t_);
-  } else if (mode_ == Mode::Brightness) {
-    if (brightness_ >= 100) {
-      brightness_ = 0;
-    } else {
-      brightness_ += 10;
-    }
-    SendBrightness(brightness_);
+  switch (mode_) {
+    case Mode::TimeMinutes:
+      time_ += 1;
+      setTime(time_);
+      SendTime(time_);
+      break;
+    case Mode::TimeHours:
+      time_ += 60;
+      setTime(time_);
+      SendTime(time_);
+      break;
+    case Mode::AlarmMinutes:
+      alarm_.alarm_time += 1;
+      SendTime(alarm_.alarm_time);
+      break;
+    case Mode::AlarmHours:
+      alarm_.alarm_time += 60;
+      SendTime(alarm_.alarm_time);
+      break;
+    case Mode::Brightness:
+      if (brightness_ >= 100) {
+        brightness_ = 0;
+      } else {
+        brightness_ += 10;
+      }
+      SendBrightness(brightness_);
+      break;
+    default:
+      break;
   }
 }
 
 void ModeFunction() {
+  switch (mode_) {
+    case Mode::Normal:
+      SendTime(time_);
+      mode_ = Mode::TimeMinutes;
+      break;
+    case Mode::TimeMinutes:
+      SendTime(time_);
+      mode_ = Mode::TimeHours;
+      break;
+    case Mode::TimeHours:
+      SendTime(alarm_.alarm_time);
+      mode_ = Mode::AlarmMinutes;
+      break;
+    case Mode::AlarmMinutes:
+      SendTime(alarm_.alarm_time);
+      mode_ = Mode::AlarmHours;
+      break;
+    case Mode::AlarmHours:
+      SendTime(time_);
+      mode_ = Mode::Brightness;
+      break;
+    case Mode::Brightness:
+      mode_ = Mode::Normal;
+      break;
+  }
   if (mode_ == Mode::Normal) {
-    mode_ = Mode::TimeMinutes;
+    
   } else if (mode_ == Mode::TimeMinutes) {
-    mode_ = Mode::TimeHours;
+    
   } else if (mode_ == Mode::TimeHours) {
-    mode_ = Mode::Brightness;
+    
   }else if (mode_ == Mode::Brightness) {
-    mode_ = Mode::Normal;
+    
   }
   Serial.print("ModeFunction, mode:");
   Serial.println((int)mode_);
@@ -85,6 +137,7 @@ void setup(){
   Wire.begin();
   pinMode(SETTINGS, INPUT_PULLUP);
   pinMode(ACTION, INPUT_PULLUP); 
+  pinMode(ALARM, INPUT_PULLUP); 
   Serial.println("This is the start of the program");
 }
 
@@ -117,9 +170,29 @@ void SendBrightness(byte brightness) {
 
 void loop(){
   time_t t = now();
-  if ((mode_ == Mode::Normal) && (t - t_ > 0)) {
+  if ((mode_ == Mode::Normal) && (t - time_ > 0)) {
     SendTime(t);
-    t_ = t;
-  }  
+    time_ = t;
+  }
+
+  if (alarm_.alarm_active && digitalRead(ALARM) == HIGH) {
+    alarm_.alarm_active = false;
+    alarm_.alarm_off = now();
+    Serial.println("Alarm is now inactive");
+  }
+
+  if (minute(time_) == minute(alarm_.alarm_time) &&
+      minute(now()) > minute(alarm_.alarm_off)) {
+    if (digitalRead(ALARM) == LOW && !alarm_.alarm_active) {
+      alarm_.alarm_active = true;
+      Serial.println("Alarm is now active");
+    } 
+  } 
+
+  if (alarm_.alarm_active && minute(now())-minute(alarm_.alarm_time) >= 1) {
+      alarm_.alarm_active = false;
+      Serial.println("Alarm is now inactive");
+  }
+
   CheckInput();
 }
