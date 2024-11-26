133ac2be5ba050cb3f7a51f961deffc5d870321f
jonas <jonasgarsten@gmail.com>
2024-11-26 20:38:20 +0100

    Clock plays music when alarm is active

--- display_with_shift_register_master/display_with_shift_register_master.ino
+++ display_with_shift_register_master/display_with_shift_register_master.ino
@@ -1,5 +1,13 @@
 #include <TimeLib.h>
 #include <Wire.h>
+#include <SoftwareSerial.h>
+#include <DFRobotDFPlayerMini.h>
+
+// For DFPlayer
+SoftwareSerial softSerial(/*rx =*/10, /*tx =*/11);
+#define FPSerial softSerial
+DFRobotDFPlayerMini myDFPlayer;
+void printDetail(uint8_t type, int value);
 
 unsigned long lastButtonPressTime = 0;
 // Wait time in ms between two button presses
@@ -31,6 +39,26 @@ struct Alarm {
 Alarm alarm_;
 void SendTime(time_t t);
 void SendBrightness(byte brightness);
+
+void setupAudio() {
+  FPSerial.begin(9600);
+
+  Serial.println();
+  Serial.println(F("DFRobot DFPlayer Mini Demo"));
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+
+  if (!myDFPlayer.begin(FPSerial, /*isACK = */true, /*doReset = */true)) {  //Use serial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true){
+      delay(0); // Code to compatible with ESP8266 watch dog.
+    }
+  }
+  Serial.println(F("DFPlayer Mini online."));
+
+  myDFPlayer.volume(15);  //Set volume value. From 0 to 30
+}
 
 class Button {
   public:
@@ -138,6 +166,7 @@ void setup(){
   pinMode(SETTINGS, INPUT_PULLUP);
   pinMode(ACTION, INPUT_PULLUP); 
   pinMode(ALARM, INPUT_PULLUP); 
+  setupAudio();
   Serial.println("This is the start of the program");
 }
 
@@ -177,6 +206,7 @@ void loop(){
 
   if (alarm_.alarm_active && digitalRead(ALARM) == HIGH) {
     alarm_.alarm_active = false;
+    myDFPlayer.pause();
     alarm_.alarm_off = now();
     Serial.println("Alarm is now inactive");
   }
@@ -185,14 +215,77 @@ void loop(){
       minute(now()) > minute(alarm_.alarm_off)) {
     if (digitalRead(ALARM) == LOW && !alarm_.alarm_active) {
       alarm_.alarm_active = true;
+      myDFPlayer.loop(2);
       Serial.println("Alarm is now active");
     } 
   } 
 
   if (alarm_.alarm_active && minute(now())-minute(alarm_.alarm_time) >= 1) {
       alarm_.alarm_active = false;
+      myDFPlayer.pause();
       Serial.println("Alarm is now inactive");
   }
 
   CheckInput();
+}
+
+void printDetail(uint8_t type, int value){
+  switch (type) {
+    case TimeOut:
+      Serial.println(F("Time Out!"));
+      break;
+    case WrongStack:
+      Serial.println(F("Stack Wrong!"));
+      break;
+    case DFPlayerCardInserted:
+      Serial.println(F("Card Inserted!"));
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println(F("Card Removed!"));
+      break;
+    case DFPlayerCardOnline:
+      Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerUSBInserted:
+      Serial.println("USB Inserted!");
+      break;
+    case DFPlayerUSBRemoved:
+      Serial.println("USB Removed!");
+      break;
+    case DFPlayerPlayFinished:
+      Serial.print(F("Number:"));
+      Serial.print(value);
+      Serial.println(F(" Play Finished!"));
+      break;
+    case DFPlayerError:
+      Serial.print(F("DFPlayerError:"));
+      switch (value) {
+        case Busy:
+          Serial.println(F("Card not found"));
+          break;
+        case Sleeping:
+          Serial.println(F("Sleeping"));
+          break;
+        case SerialWrongStack:
+          Serial.println(F("Get Wrong Stack"));
+          break;
+        case CheckSumNotMatch:
+          Serial.println(F("Check Sum Not Match"));
+          break;
+        case FileIndexOut:
+          Serial.println(F("File Index Out of Bound"));
+          break;
+        case FileMismatch:
+          Serial.println(F("Cannot Find File"));
+          break;
+        case Advertise:
+          Serial.println(F("In Advertise"));
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
 }
